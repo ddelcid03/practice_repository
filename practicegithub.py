eec08f996096bf48f8e323d302ba113b6b6f93c9
Diego Delcid <diegosdelcid@gmail.com>
2024-03-25 19:58:58 -0400

    Create practicegithub.py

---
+++ practicegithub.py
@@ -0,0 +1,10 @@
+def real(person):
+    person_answer = input("Yes or no?")
+    if person_answer == "Yes":
+        return True
+    else:
+        return False 
+    
+    
+answer = real("Diego")
+print(real)
